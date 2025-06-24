145bef3fe59ea824db72978e122662e755850676
Alisson Barreto <99barretoalisson@gmail.com>
2025-06-23 21:36:47 -0300

    Finalizando correção e atualizando Projeto

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,7 +1,10 @@
+﻿#include <windows.h>
+#include <locale.h>
 #include <stdio.h>
 
 int main() {
-  int populacao, pontoturistico;
+system("chcp 65001");
+int populacao, pontoturistico;
   char estadoA;
   char codigocarta [50];
   char nomecidade [50];
@@ -38,7 +41,44 @@ printf("Nome Cidade: %s\n", nomecidade);
 
 printf("PIB: %f bilhões de reais\n", pib);
 printf("Área: %f km²\n", area);
+  
+  int populacao2, pontoturistico2;
+  char estadoB;
+  char codigocarta2 [50];
+  char nomecidade2 [50];
+  float area2, pib2;
 
+printf ("Digite a população da carta:\n");
+scanf ("%d", &populacao2);
+
+printf("Digite o Ponto Turistico da carta:\n");
+scanf ("%d", &pontoturistico2);
+
+printf("Digite o Estado da carta:\n");
+scanf("%c", &estadoB);
+
+printf("Digite o Código da carta:\n");
+fgets(codigocarta2, sizeof(codigocarta2), stdin);
+
+printf("Digite o Nome da cidade:\n");
+fgets(nomecidade2, sizeof(nomecidade2), stdin);
+
+printf("Digite a Área da cidade:\n");
+scanf("%f, &area2");
+
+printf("Digite o PIB da cidade:\n");
+scanf("%f", &pib2);
+
+printf("Carta 2:\n");
+printf("População: %d\n", populacao2);
+printf("Ponto Turistico: %d\n",pontoturistico2);
+
+printf("Estado: %c\n", estadoB);
+printf("Código da Carta: %s\n", codigocarta2);
+printf("Nome Cidade: %s\n", nomecidade2);
+
+printf("PIB: %f bilhões de reais\n", pib2);
+printf("Área: %f km²\n", area2);
 
 }
 
